a42c8e2255c35cc58f38fd81573f7cc038244d96
santiper5 <smartin0599@gmail.com>
2023-04-20 18:25:12 -0300

    Se avanza un poco

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,6 +7,10 @@ class Home extends BaseController
     public function index()
     {
         return view('principal.html');
+        echo view('plantillas/quienes');
+    }
+    public function contacto(){
+
     }
 }
 
