d451aebad17ad70443d3c4d31153f83e421b4224
santiper5 <smartin0599@gmail.com>
2023-04-20 19:10:50 -0300

    id03

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,11 +7,27 @@ class Home extends BaseController
     public function index()
     {
         return view('principal.html');
-        echo view('plantillas/quienes');
     }
     public function contacto(){
-
+        echo view('componentes//header.html');
+        echo view("componentes//navbar.html");
+        echo view("componentes//footer.html");
+    }
+    public function quienes_somos(){
+        echo view('componentes//header.html');
+        echo view("componentes//navbar.html");
+        echo view("componentes//footer.html");
+    }
+    public function comercializacion(){
+        echo view('componentes//header.html');
+        echo view("componentes//navbar.html");
+        echo view("componentes//footer.html");
+    }
+    public function term_usos(){
+        echo view('componentes//header.html');
+        echo view("componentes//navbar.html");
+        echo view("componentes//footer.html");
     }
 }
 
-
+?>
